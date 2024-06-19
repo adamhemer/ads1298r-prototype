91299914a5d331b79a5f05fd617e41df36e66973
Adam Hemer <adamhemer32@gmail.com>
2024-06-19 10:12:16 +0930

    Single channel reading working without noise.

---
+++ ads1298rserial.m
@@ -0,0 +1,40 @@
+serial = serialport("COM7", 115200);
+
+received = 0;
+
+x = 1:1:2000;
+y = zeros(2000, 4);
+
+h = figure;
+
+max = 2 ^ 24 - 1;
+ylims = [-max, max];
+
+updatePeriod = 0;
+collected = 0;
+
+while ishandle(h)
+    if serial.NumBytesAvailable > 0
+        line = serial.readline();
+        data = split(line, ", ");
+        data(end) = [];
+        if length(data) == 4
+            received = received + 1;
+            y = circshift(y, 1);
+            y(1,:) = data';
+        end
+
+        updatePeriod = updatePeriod - 1;
+        if updatePeriod < 0
+            updatePeriod = 1000;
+            plot(x, y)
+            %ylim(ylims)
+            drawnow
+        end
+    end
+    
+end
+
+
+
+delete(serial)
